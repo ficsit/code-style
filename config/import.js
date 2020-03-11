8318efed873b322db0c361d07542aa6ad0233d82
Ian MacLeod <ian@nevir.net>
2020-03-11 01:05:09 -0700

    import plugin

--- config/import.js
+++ config/import.js
@@ -2,4 +2,33 @@
 module.exports = {
   plugins: ['import'],
   extends: ['plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
+  overrides: [
+    {
+      files: ['**.{js,jsx,ts,tsx}'],
+
+      // https://github.com/alexgorbatchev/eslint-import-resolver-typescript
+      'settings': {
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+          'typescript': {
+            'alwaysTryTypes': true,
+          },
+        },
+      },
+
+      rules: {
+        'import/order': [
+          'error',
+          {
+            groups: [['external', 'builtin'], 'internal', 'parent', 'sibling', 'index'],
+            pathGroups: [{ 'pattern': '~/**', 'group': 'internal' }],
+            'newlines-between': 'always',
+            'alphabetize': { order: 'asc' },
+          },
+        ],
+      },
+    },
+  ],
 };
